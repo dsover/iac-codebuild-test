5f45b2337c5308d6f84a164e9b3cf0dc08d767bc
Alex Sover <alex.sover@turner.com>
2021-01-27 17:04:48 -0500

    wip

--- build/application-infrastructure/data.tf
+++ build/application-infrastructure/data.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "../lambda/handler"
-  output_path = "../lambda/dist/handler.zip"
+  source_dir  = "../../lambda/handler"
+  output_path = "../../lambda/dist/handler.zip"
 }
 
 data "aws_caller_identity" "current" {}
