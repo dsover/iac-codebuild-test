70e94e34eec4668525352ec1da6589a3192f9578
Alex Sover <alex.sover@turner.com>
2021-01-28 16:52:17 -0500

    wip

--- build/codebuild-infrastructure/main.tf
+++ build/codebuild-infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.13"
   backend "s3" {
     region         = "us-east-1"
-    bucket         = "tf-state-zion-aws-turner-playground"
+    bucket         = "tf-state-zion-playground"
     key            = "terraform.tfstate"
   }
 }
