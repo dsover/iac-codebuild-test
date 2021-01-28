3ae70bfb40f4700c91cd3a91caa4ca9f513907ad
Alex Sover <alex.sover@turner.com>
2021-01-28 13:18:12 -0500

    wip

--- lambda/handler/index.js
+++ lambda/handler/index.js
@@ -4,7 +4,7 @@ function write(text){
 }
 
 exports.handler = async (event, context) => {
-    write('Hello World with a change to permissions for chris');
+    write('Hello World I can change!!!');
     return;
 };
 exports.write = write;
