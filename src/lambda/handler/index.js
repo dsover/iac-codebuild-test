3739dd92086c271b481b6c01e628c96f657a6119
Alex Sover <alex.sover@turner.com>
2021-01-29 15:49:22 -0500

    chore: update lambda code

--- src/lambda/handler/index.js
+++ src/lambda/handler/index.js
@@ -4,7 +4,7 @@ function write(text){
 }
 
 exports.handler = async (event, context) => {
-    write('Hello World, This is how it started');
+    write('Hello World, This is how it ended!!!');
     return;
 };
 exports.write = write;
